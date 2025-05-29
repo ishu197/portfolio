09b56e17825bd6e3704cd305aebeb580a7b2d499
ishu197 <iswaryanainar041@gmail.com>
2025-05-29 10:49:03 +0530

    first commit

---
+++ basics.py
@@ -0,0 +1,50 @@
+# print("hello")
+# today_class="python"
+# print(today_class)
+# x=y=z=12.3
+# a=b=c=32.1
+# print(x,y,z,a,b,c)
+# m=100 
+# n=float(m)
+# print
+# slicing
+# 
+# aim = "web developer"
+# print(f"my aim is {aim}")12
+# a=int(input("enter the value"))
+# print(a)
+# b=float(input("enter the value"))
+# print(b)
+# f=input("enter the value")
+# c=str(a+b)+f
+# print(c)
+# 
+#
+# 
+# 
+# 
+# b=15
+# j=16
+# c=(b!=j)and(b<j)
+# print(c)
+# ihgkh
+# dtyb
+
+# if h == k:
+#     print("h is equal to k")
+# elif h>k:
+#     print("h is greater than k")
+# else :
+#     print("h is less than k")
+# if
+# 
+# print("royal enfield")
+# print("r15")
+# print("rx100")
+# bike=input("enter the prize")
+# if bike=="royal enfiekd":
+
+#     print(200000)
+# elif bike =="r15":
+#     print(150000)
+
